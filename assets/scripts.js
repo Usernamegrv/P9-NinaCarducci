9c744f670d0fb7baf082505f9c5ebe1c3155d0e5
Usernamegrv <139330806+Usernamegrv@users.noreply.github.com>
2024-01-20 12:12:01 +0100

    catch error + defer + taille img

--- assets/scripts.js
+++ assets/scripts.js
@@ -1,15 +1,24 @@
-$(document).ready(function() {
-    $('.gallery').mauGallery({
-        columns: {
-            xs: 1,
-            sm: 2,
-            md: 3,
-            lg: 3,
-            xl: 3
-        },
-        lightBox: true,
-        lightboxId: 'myAwesomeLightbox',
-        showTags: true,
-        tagsPosition: 'top'
+$(document).ready(function () {
+  try {
+    $(".gallery").mauGallery({
+      columns: {
+        xs: 1,
+        sm: 2,
+        md: 3,
+        lg: 3,
+        xl: 3,
+      },
+      lightBox: true,
+      lightboxId: "myAwesomeLightbox",
+      showTags: true,
+      tagsPosition: "top",
     });
+  } catch (error) {
+    console.error(
+      "une erreur s'est produite lors de la configuration de la galerie:",
+      error
+    );
+  }
 });
+
+// ajout gestion erreur
